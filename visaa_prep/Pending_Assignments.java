128b0885becc0b116c1978143616057e152dd167
Shirisha988 <vislavathshirisha08@gmail.com>
2024-11-18 21:51:15 +0530

    Create Pending_Assignments.java

---
+++ visaa_prep/Pending_Assignments.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        System.out.println((x*y)<=((z*24)*60)?"YES":"NO");
+        sc.close();
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
